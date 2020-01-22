c720e7d2a2728b30174689082a16d7ee620277fa
Poviakalo <poviasha@gmail.com>
2020-01-23 01:06:58 +0200

    #1

---
+++ js/main.js
@@ -0,0 +1,23 @@
+'use strct';
+(function($) {
+    $(document).ready(function() {
+        document.querySelector('.btn').addEventListener('click', () => {
+            alert('good');
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });
+})(jQuery);
